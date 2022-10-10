36865d612a5755f33f4e35e593667d8ce4a2b21c
Voylx <channatt.m@ku.th>
2022-10-10 16:29:50 +0700

    db

--- src/services/db.js
+++ src/services/db.js
@@ -2,9 +2,10 @@ const mysql = require("mysql2");
 
 // create the connection to database
 const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  database: "database-csproject",
+  host: "sql.freedb.tech",
+  user: "freedb_anas_title",
+  password: "3a5RU2!hnfAKd5M",
+  database: "freedb_cs-project",
   multipleStatements: true,
 });
 
