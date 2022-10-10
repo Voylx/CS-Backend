a8adedbefde8bd2450108bedd91e22104746f183
Voylx <channatt.m@ku.th>
2022-10-10 18:47:18 +0700

    edit db 10-10 18:47

--- src/services/db.js
+++ src/services/db.js
@@ -2,11 +2,11 @@ const mysql = require("mysql2");
 
 // create the connection to database
 const db = mysql.createConnection({
-  host: "sql.freedb.tech",
-  user: "freedb_anas_title",
-  password: "3a5RU2!hnfAKd5M",
-  database: "freedb_cs-project",
-  multipleStatements: true,
+  host: "qvti2nukhfiig51b.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+  user: "jnxbez02f9mr80mo",
+  password: "ezxuyhsrstmjd21j",
+  database: "bp31kt8v8vsp23nn",
+  // multipleStatements: true,
 });
 
 module.exports = db;
